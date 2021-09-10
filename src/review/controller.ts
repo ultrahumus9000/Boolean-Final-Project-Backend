9ba2ad2b4164ad2c2310f0ffb0adb9b397d6f20e
Linlin Li <paris1900lll@gmail.com>
2021-09-10 18:18:53 +0100

    x

--- src/review/controller.ts
+++ src/review/controller.ts
@@ -2,18 +2,35 @@ import { User } from "@prisma/client";
 import { Request, Response } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import db from "../database";
-const { review } = db;
+const { review, user } = db;
 async function createNewReview(req: Request, res: Response) {
   const { id } = req.currentUser as User;
   //req.body only need content and houseId
-  const newReview = req.body;
-  console.log(newReview);
   try {
+    const guestInfo = await user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        guestProfile: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    const realGuestId = guestInfo?.guestProfile?.id;
+    if (realGuestId === undefined) {
+      return;
+    }
+
+    const newReview = req.body;
+
     const newReviewResult = await review.create({
       data: {
         content: newReview.content,
         houseId: newReview.houseId,
-        guestId: id,
+        guestId: realGuestId,
       },
     });
     res.json(newReviewResult);
